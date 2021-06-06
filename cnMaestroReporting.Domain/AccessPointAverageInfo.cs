9f71e918db28deb361db1213096771a1632db57c
Chris Chance <chrischance@outlook.com>
2021-06-06 16:04:19 -0400

    Refactoring and Progress Indicators, as well as some missing radios

---
+++ cnMaestroReporting.Domain/AccessPointAverageInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace cnMaestroReporting.Domain
+{
+    public struct AccessPointAverageInfo
+    {
+        public string ApName { get; set; }
+        public string Tower { get; set; }
+        public int SMs { get; set; }
+        public int AvgSmDistanceM { get; set; }
+        public int MaxSmDistanceM { get; set; }
+        public int AvgApPl { get; set; }
+        public int WorstApPl { get; set; }
+        public int AvgSmPl { get; set; }
+        public int WorstSmPl { get; set; }
+        public int AvgSmSnrH { get; set; }
+        public int AvgSmSnrV { get; set; }
+        public int WorstSmSnr { get; set; }
+        public int AvgApSnrH { get; set; }
+        public int AvgApSnrV { get; set; }
+        public int WorstApSnr { get; set; }
+    }
+}
---
+++ cnMaestroReporting.SNMP/SnmpProgress.cs
@@ -0,0 +1,12 @@
+﻿namespace cnMaestroReporting.SNMP
+{
+    public class SnmpProgress
+    {
+        //current progress
+        public int CurrentProgressAmount { get; set; }
+        //total progress
+        public int TotalProgressAmount { get; set; }
+        //Message
+        public string CurrentProgressMessage { get; set; }
+    }
+}
